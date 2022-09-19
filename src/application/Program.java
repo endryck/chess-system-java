27d2b00ba55950647c2ba580c35ab09350dfddf5
Matheus Endryck <matheusendryck@hotmail.com>
2022-09-19 16:59:28 -0300

    Created Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-		System.out.println("Hello");
-
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
