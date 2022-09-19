7eec95107eb2d6552a93d1b2dd6b6d59a88001ca
Matheus Endryck <matheusendryck@hotmail.com>
2022-09-19 18:38:02 -0300

    Created Rook and King

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,8 +16,8 @@ public class ChessMatch {
 	}
 	
 	public ChessPiece[][] getPieces(){
-		ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumns()];
-		for(int i=0; i<board.getRow(); i++) {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for(int i=0; i<board.getRows(); i++) {
 			for (int j=0; j<board.getColumns(); j++) {
 				mat[i][j] =(ChessPiece) board.piece(i, j);
 			}
@@ -25,7 +25,7 @@ public class ChessMatch {
 		return mat;
 	}
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(9, 1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));	
 		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
